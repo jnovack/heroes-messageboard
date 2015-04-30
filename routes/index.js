78a62090216da1d5fce81da1bf13d02686bc4b3e
Justin J. Novack <jnovack@parxcasino.com>
2015-04-30 17:12:36 -0400

    staging - end-of-match mockup

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,10 @@ router.get('/', function(req, res, next) {
     res.sendFile('index.html', { root: publicDir });
 });
 
+router.get('/end-of-match', function(req, res, next) {
+    res.render('end-of-match.jade', { esl: true } );
+});
+
 router.get('/admin', function(req, res, next) {
     res.render('admin', { title: 'Admin Interface'});
 });
